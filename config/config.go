0a620d37e2b98554060672d32264f2fa1c69d689
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2024-01-12 22:11:47 +0700

    chore: delete unuseful file and resources meant as an example

--- config/config.go
+++ config/config.go
@@ -1,6 +1,5 @@
 package config
 
 const ProjectID string = "glutara"
-const PostCollection string = "posts"
 const UserCollection string = "users"
 const ReminderCollection string = "reminders"
--- router/router.go
+++ router/router.go
@@ -10,11 +10,6 @@ import (
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
-	// Post Handler
-	router.HandleFunc("/api", middleware.MainHandler).Methods("GET")
-	router.HandleFunc("/api/posts-get", middleware.GetPosts).Methods("GET")
-	router.HandleFunc("/api/posts-post", middleware.AddPost).Methods("POST")
-
 	// Auth Handler
 	router.HandleFunc("/api/auth/register", middleware.Register).Methods("POST")
 	router.HandleFunc("/api/auth/login", middleware.Login).Methods("POST")
