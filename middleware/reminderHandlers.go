2a7528c367a8e41d56bccf9c5559049d7c2b187f
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2024-01-12 21:56:28 +0700

    feat: handlers to get, create, and delete user reminder

---
+++ middleware/reminderHandlers.go
@@ -0,0 +1,84 @@
+package middleware
+
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/gorilla/mux"
+	"strconv"
+
+	"glutara/repository"
+	"glutara/models"
+)
+
+var (
+	reminderRepo repository.ReminderRepository = repository.NewReminderRepository()
+)
+
+func GetReminders(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(request)
+	userID, err := strconv.ParseInt(vars["UserID"], 10, 64)
+	if err != nil {
+		http.Error(response, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+	
+	reminders, err := reminderRepo.FindAllUserReminders(userID)
+	if err != nil {
+		http.Error(response, "Failed to retrieve reminders data", http.StatusInternalServerError)
+		return
+	}
+
+	response.WriteHeader(http.StatusOK)
+	json.NewEncoder(response).Encode(reminders)
+}
+
+func CreateReminder(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("Content-Type", "application/json")
+	var reminder models.Reminder
+	vars := mux.Vars(request)
+	userID, err := strconv.ParseInt(vars["UserID"], 10, 64)
+	if err != nil {
+		http.Error(response, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	err = json.NewDecoder(request.Body).Decode(&reminder)
+	if err != nil {
+		http.Error(response, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	max, err := reminderRepo.GetUserRemindersMaxCount(userID)
+	if err != nil {
+		http.Error(response, "Failed to get max reminder count", http.StatusInternalServerError)
+		return
+	}
+	reminder.ReminderID = max + 1
+
+	reminderRepo.Save(&reminder)
+
+	response.WriteHeader(http.StatusOK)
+	json.NewEncoder(response).Encode(reminder)
+}
+
+func DeleteReminder(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	userID, err := strconv.ParseInt(vars["UserID"], 10, 64)
+	if err != nil {
+		http.Error(response, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+	reminderID, err := strconv.ParseInt(vars["ReminderID"], 10, 64)
+	if err != nil {
+		http.Error(response, "Invalid reminder ID", http.StatusBadRequest)
+		return
+	}
+
+	err = reminderRepo.Delete(userID, reminderID)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	response.WriteHeader(http.StatusOK)
+}
